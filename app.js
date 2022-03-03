8b8715569134d1736969836c9f170f5d8fe55ffc
Raselraaz123 <raselraaz91@gmail.com>
2022-03-03 02:28:12 -0800

    secnd

---
+++ app.js
@@ -0,0 +1,15 @@
+function nom() {
+    console.log('Eating rice.')
+}
+
+function somethingInSide() {
+    console.log("Home : please don't go")
+}
+
+console.log('frist:go')
+
+console.log('second:went')
+setTimeout(somethingInSide,5000);
+console.log('three:Gone')
+setTimeout(nom);
+console.log('go to Newmarket')
